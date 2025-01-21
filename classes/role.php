6aec246d14097829f9f98bc81af91b8c6f35dea0
bourraOmar <bwrtmr@gmail.com>
2025-01-21 16:09:53 +0100

    add class role

--- classes/role.php
+++ classes/role.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 require_once '../classes/conn.php';
 
-class role {
+class role
+{
     private $id;
     private $name;
 
-    function __construct($name){
+    function __construct($name)
+    {
         $this->name = $name;
     }
 
-    static function getallroles(){
+    static function getallroles()
+    {
         $db = Dbconnection::getInstance()->getConnection();
-        try{
-        $sql = "SELECT * FROM role WHERE role_id <> 1";
-        $stmt = $db->prepare($sql);
-        $stmt->execute();
+        try {
+            $sql = "SELECT * FROM role WHERE role_id <> 1";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
 
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-        catch(PDOException $e){
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
             throw new Exception('there is an error in roles show ' . $e->getMessage());
         }
     }
 }
-
-?>
